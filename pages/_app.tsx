56517edd25da08e8228177c3f0e3d8cda78ef7f9
B-jisoo <mrk334kr@naver.com>
2022-11-14 06:27:02 +0900

    redux 주석

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/globals.css";
 import { getClient } from "../queryClient";
 import { Provider } from "react-redux";
-import store from "../redux/store";
+// import store from "../redux/store";
 
 export const App = ({ Component, pageProps }: AppProps) => {
   const queryClient = getClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Provider>
+      {/* <Provider store={store}> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      {/* </Provider> */}
     </QueryClientProvider>
   );
 };
